925e5a4c73971b8955178866adcbd68c1a7d8910
Amanda McBee <ahanes35@yahoo.com>
2022-03-25 12:02:27 -0600

    Works

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,27 @@
 import './App.css';
-// import PortfolioContainer from './components/portfolioContainer'
 import Contact from './components/Contact/Contact';
-import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-// import logo from '../src/assets/weed.png';
 import Home from './components/Home/Home';
-import Order from './components/Order/Order';
 import Menu from './components/Menu/Menu';
 import Member from './components/Member/Member';
-
-import { useState } from 'react';
+import Entry from './components/Entry/Entry';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'Order') {
-      return <Order />;
-    }
-    if (currentPage === 'Menu') {
-      return <Menu />;
-    }
-    if (currentPage === 'Member') {
-      return <Member />
-    }
-    return <Contact />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div className="main">
-      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-      <div className="portfolio-container">{renderPage()}</div>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Entry />} />
+      </Routes>
       <Footer />
     </div>
   );
 
-
-
-  // return (
-  //   <div className="wrapper">
-  //     <h1>
-  //     <Header />
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p className="fsdTitle">
-  //           Frozen Smoke Dispensary
-  //         </p>
-  //         <p>
-  //           500 US-50 #103, Gunnison, CO 81230
-  //         </p>
-  //         <br/>
-  //         <Contact />
-  //         <Footer />
-  //       </header>
-  //     </h1>
-  //   </div>
-  // )
 };
 
 export default App;
