e5629dea595b4e777754c7d10b20d1415495d2a9
Amanda McBee <ahanes35@yahoo.com>
2022-03-25 12:37:44 -0600

    Added 'sorry' response to no button click on enrty

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
 import Member from './components/Member/Member';
 import Entry from './components/Entry/Entry';
+import Sorry from './components/Sorry/Sorry';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="main">
       <Routes>
+        <Route path="/sorry" element={<Sorry />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/member" element={<Member />} />
---
+++ client/src/components/Sorry/Sorry.js
@@ -0,0 +1,10 @@
+import "./Sorry.css";
+
+export default function Sorry() {
+
+  return (
+    <div>
+      Sorry, you must be 21 or older to view this page.
+    </div>
+  );
+}
