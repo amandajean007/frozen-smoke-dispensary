814f9fa043cc927e5f34d8f8e9948440c0cd83ba
Amanda McBee <ahanes35@yahoo.com>
2022-03-27 16:55:47 -0600

    database coming together, controllers working

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -8,22 +8,24 @@ const Member = require('../models/memberModel');
 const getMembers = asyncHandler(async (req, res) => {
     const members = await Member.find()
 
-    res.status(200).json(members)
+    res.status(200).json(members);
+    // console.log(members);
 });
 
 // @desc    Create Members
 // @route   POST  /api/members
 // @access  Private
 const createMember = asyncHandler(async (req, res) => {
-    console.log("req.body.text " + req.body.text);
-    
-    if(!req.body.text) {
+    if(!req.body.name) {
         res.status(400)
         throw new Error('Please add text to the field')
     };
 
     const member = await Member.create({
-        text: req.body.text
+        name: req.body.name,
+        dob: req.body.dob,
+        email: req.body.email,
+        phone: req.body.phone
     })
 
     res.status(200).json(member)
@@ -33,14 +35,34 @@ const createMember = asyncHandler(async (req, res) => {
 // @route   PUT  /api/members/:id
 // @access  Private
 const updateMember = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update member ${req.params.id}` })
+    const member = await Member.findById(req.params.id)
+
+    if(!member) {
+        res.status(400)
+        throw new Error('Member not found')
+    }
+
+    const updateMember = await Member.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updateMember)
 });
 
 // @desc    Delete Member
 // @route   DELETE  /api/members/:id
 // @access  Private
 const deleteMember = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete member ${req.params.id}` })
+    const member = await Member.findById(req.params.id)
+
+    if(!member) {
+        res.status(400)
+        throw new Error('Member not found')
+    }
+
+    await member.remove()
+    
+    res.status(200).json({ id: req.params.id })
 });
 
 module.exports = {
