017bda8b450eb0b75bf9bfaea042711f1cc9ae11
Amanda McBee <ahanes35@yahoo.com>
2022-03-31 15:32:24 -0600

    promo cards react components

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import "./Home.css";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Promo from '../Promo/Promo';
 // import store from '../../assets/FSD_Storefront.png';
 import yummies from '../../assets/yummies.png';
+import starjoint from '../../assets/icon-joint.png'
 
 export default function Home() {
 
@@ -15,24 +17,46 @@ export default function Home() {
       </div>
 
       <div className="promoContainer">
-
-        <div id="gummyPromo" className="promoGummies">
-          <a className="glow" href="/menu">
-            Yummies Gummies 2 for $30!<br />
-            <div className="centter">
-              <img src={yummies} width="75%" height="auto" className="promoImage" alt="yummies"></img>
-            </div>
-          </a>
-        </div>
-
-        <div id="jointPromo" className="promoJoint">
-          <a className="glow" href="https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,3,,," target="_blank">
-            Leave a Five-Star Google Review, get a free joint!<br />🌟🌟🌟🌟🌟
-            <div className="centter">
-              <img src={yummies} width="75%" height="auto" className="promoImage" alt="yummies"></img>
-            </div>
-          </a>
-        </div>
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={yummies}
+          promoText={"Yummies Gummies 2 for $30!"}
+          promoHref={"/menu"}
+        />
+        <Promo
+          promoImg={starjoint}
+          promoText={"🌟🌟🌟🌟🌟 Leave a Five-Star Google Review, get a free joint!"}
+          promoHref={"https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,3,,,"}
+        />
 
       </div>
 
