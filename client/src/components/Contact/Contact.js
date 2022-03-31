b6d2db86cd3fae4fc3955450accfe052ab0c9e2a
Amanda McBee <ahanes35@yahoo.com>
2022-03-30 20:34:30 -0600

    contact form

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -25,10 +25,12 @@ function Contact() {
   return (
     <div className="contact">
       <Header />
-      <h1>Contact Us</h1>
-      <div className="contactCard">
-        <p>(970)672-3495</p>
-        <p>500 US-50 #103, Gunnison, CO 81230</p>
+      <div className="reference">
+        <div className="contactCard">
+            <a href="tel:9706723495">(970)672-3495</a><br/>
+            <a target="_blank" rel="noreferrer" href="mailto:frozensmokedispensary@gmail.com">frozensmokedispensary@gmail.com</a><br/>
+            <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/dir//frozen+smoke+dispensary/@38.5411617,-106.9426253,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x873f92e6665ea4c9:0xd60874f3d9ec7e4b!2m2!1d-106.9404632!2d38.5393374">500 US-50 #103, Gunnison, CO 81230</a>
+        </div>
       </div>
       <Footer />
     </div>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,7 +11,12 @@ export default function Home() {
         <div className="googleReviews">
           <a href="https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,1,,," target="_blank">Google Reviews</a>
         </div>
+        <div className="googleReviews">
+          <a href="
+https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,3,,," target="_blank">Leave a Five-Star Review, get a free joint!</a>
+        </div>
         <Footer />
     </div>
   );
 }
+
