8295fc02548e9c27cf99093dbc176137a45626f7
Amanda McBee <ahanes35@yahoo.com>
2022-04-03 16:31:28 -0600

    werking

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -21,21 +21,14 @@ export default function Home() {
           promoText={"Yummies Gummies 2 for $30!"}
           promoHref={"/menu"}
         />
-        <Promo
-          promoImg={yummies}
-          promoText={"Yummies Gummies 2 for $30!"}
-          promoHref={"/menu"}
-        />
-        <Promo
-          promoImg={yummies}
-          promoText={"Yummies Gummies 2 for $30!"}
-          promoHref={"/menu"}
-        />
-        <Promo
-          promoImg={starjoint}
-          promoText={"🌟🌟🌟🌟🌟 Leave a Five-Star Google Review, get a free joint!"}
-          promoHref={"https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,3,,,"}
-        />
+        <div className="promo">
+          <a className="glow" href="https://www.google.com/search?q=frozen+smoke+dispensary&rlz=1C5CHFA_enUS991US991&oq=frozen+smoke+dispensary&aqs=chrome..69i57j46i175i199i512j0i512j46i512j0i512j0i10i512l2j0i22i30l3.3905j0j15&sourceid=chrome&ie=UTF-8#lrd=0x873f92e6665ea4c9:0xd60874f3d9ec7e4b,3,,,">
+          🌟🌟🌟🌟🌟<br/>Leave a Five-Star Google Review, get a free joint!
+            <div className="centter">
+              <img src={starjoint} width="75%" height="auto" className="promoImage" alt="promoImg"></img>
+            </div>
+          </a>
+        </div>
 
       </div>
 
