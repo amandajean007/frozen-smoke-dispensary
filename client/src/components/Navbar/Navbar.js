fb311fe134a95288ba61ed2180f2bd4b55d01c9f
Amanda McBee <ahanes35@yahoo.com>
2022-04-04 12:14:10 -0600

    ready for menu

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -46,23 +46,23 @@ function NavBar() {
                         Brands
                     </Link>
                     <div className="dropdown-content">
-                        <Link to='/Cheeba' className="dropdownLink">Cheeba Chews</Link>
-                        <Link to='/Clear' className="dropdownLink">The Clear</Link>
-                        <Link to='/Dabba' className="dropdownLink">Dabba</Link>
-                        <Link to='/Dixie' className="dropdownLink">Dixie</Link>
-                        <Link to='/Eureka' className="dropdownLink">Eureka</Link>
-                        <Link to='/GHornet' className="dropdownLink">Green Hornet</Link>
-                        <Link to='/Kahna' className="dropdownLink">Kahna</Link>
-                        <Link to='/Spherex' className="dropdownLink">Spherex</Link>
-                        <Link to='/Yummies' className="dropdownLink">Yummies</Link>
+                        <Link to='/brands/Cheeba' className="dropdownLink">Cheeba Chews</Link>
+                        <Link to='/brands/Clear' className="dropdownLink">The Clear</Link>
+                        <Link to='/brands/Dabba' className="dropdownLink">Dabba</Link>
+                        <Link to='/brands/Dixie' className="dropdownLink">Dixie</Link>
+                        <Link to='/brands/Eureka' className="dropdownLink">Eureka</Link>
+                        <Link to='/brands/GHornet' className="dropdownLink">Green Hornet</Link>
+                        <Link to='/brands/Kahna' className="dropdownLink">Kahna</Link>
+                        <Link to='/brands/Spherex' className="dropdownLink">Spherex</Link>
+                        <Link to='/brands/Yummies' className="dropdownLink">Yummies</Link>
                     </div>
                 </li>
                 <li className="nav-item dropdown">
                     <Link to='/contact' className='nav-link'>Contact Us</Link>
                     <div className="dropdown-content">
-                        <a className="dropdownLink" target="_blank" href="https://www.google.com/maps/dir//frozen+smoke+dispensary/@38.5411617,-106.9426253,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x873f92e6665ea4c9:0xd60874f3d9ec7e4b!2m2!1d-106.9404632!2d38.5393374">Directions</a>
-                        <a className="dropdownLink" mailto="frozensmokedispensary@gmail.com">frozensmokedispensary@gmail.com</a>
-                        <a className="dropdownLink" to="9706723495">(970) 672-3495</a>
+                        <a href="tel:9706723495">(970)672-3495</a><br />
+                        <a target="_blank" rel="noreferrer" href="mailto:frozensmokedispensary@gmail.com">frozensmokedispensary@gmail.com</a><br />
+                        <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/dir//frozen+smoke+dispensary/@38.5411617,-106.9426253,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x873f92e6665ea4c9:0xd60874f3d9ec7e4b!2m2!1d-106.9404632!2d38.5393374">500 US-50 #103, Gunnison CO 81230</a>
                     </div>
                 </li>
             </div>
