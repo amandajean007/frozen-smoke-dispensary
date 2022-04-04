f3ebd04c9e532633943e35dbdad78387a228c112
Amanda McBee <ahanes35@yahoo.com>
2022-04-04 12:25:46 -0600

    get/ to get*

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5001;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
