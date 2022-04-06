6f2b8ee8c9462f9a7647d08fce2c91ec80188512
Amanda McBee <ahanes35@yahoo.com>
2022-04-06 14:09:13 -0600

    small fixes

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -6,15 +6,13 @@ function Contact() {
   return (
     <div className="contact">
       <Header />
-      <div className="reference">
-        <div className="contactCard">
-            <a id="telephone" href="tel:9706723495">(970)672-3495</a><br/>
-            <a id="telephone" target="_blank" rel="noreferrer" href="mailto:frozensmokedispensary@gmail.com">frozensmokedispensary@gmail.com</a><br/>
-            <a id="telephone" target="_blank" rel="noreferrer" href="https://www.google.com/maps/dir//frozen+smoke+dispensary/@38.5411617,-106.9426253,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x873f92e6665ea4c9:0xd60874f3d9ec7e4b!2m2!1d-106.9404632!2d38.5393374">500 US-50 #103, Gunnison CO 81230</a>
+      <div className="row" id="map">
+        <div className="column">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d6241.248032559161!2d-106.94072257352772!3d38.5424343185077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x873f92e6665ea4c9%3A0xd60874f3d9ec7e4b!2sfrozen%20smoke%20dispensary!3m2!1d38.5393374!2d-106.9404632!5e0!3m2!1sen!2sus!4v1649274483992!5m2!1sen!2sus" width="900" height="675"></iframe>
         </div>
       </div>
       <Footer />
-    </div>
+    </div >
   );
 }
 
