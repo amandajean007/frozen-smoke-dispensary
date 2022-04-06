569f9427e6594e857cb6f5acb7874f2fc191a953
Amanda McBee <ahanes35@yahoo.com>
2022-04-06 13:12:43 -0600

    home page promo cards

--- client/src/components/Promo/Promo.js
+++ client/src/components/Promo/Promo.js
@@ -6,9 +6,11 @@ export default function Promo({ promoText, promoImg, promoHref }) {
   return (
     <div className="promo">
       <Link className="glow" to={promoHref}>
-        {promoText}<br />
-        <div className="centter">
-          <img src={promoImg} width="75%" height="auto" className="promoImage" alt="promoImg"></img>
+        <div className="centter" id="glow">
+          <img src={promoImg} className="promoImage" alt="promoImg"></img>
+        </div>
+        <div className="whiteText">
+          {promoText}
         </div>
       </Link>
     </div>
