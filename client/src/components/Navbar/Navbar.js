b10033c24214d898fd42972dfa1b8eb22a8a3f20
Amanda McBee <ahanes35@yahoo.com>
2022-04-06 13:20:48 -0600

    minor css changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -37,11 +37,6 @@ function NavBar() {
                     </div>
                 </li>
                 <li className="nav-item dropdown">
-                    <Link to='/member' className='nav-link dropbtn'>
-                        Membership
-                    </Link>
-                </li>
-                <li className="nav-item dropdown">
                     <Link to='/brands' className='nav-link'>
                         Brands
                     </Link>
@@ -58,12 +53,17 @@ function NavBar() {
                     </div>
                 </li>
                 <li className="nav-item dropdown">
+                    <Link to='/member' className='nav-link dropbtn'>
+                        Membership
+                    </Link>
+                </li>
+                <li className="nav-item dropdown">
                     <Link to='/contact' className='nav-link'>Contact Us</Link>
-                    <div className="dropdown-content">
+                    {/* <div className="dropdown-content">
                         <a href="tel:9706723495">(970)672-3495</a><br />
                         <a target="_blank" rel="noreferrer" href="mailto:frozensmokedispensary@gmail.com">frozensmokedispensary@gmail.com</a><br />
                         <a target="_blank" rel="noreferrer" href="https://www.google.com/maps/dir//frozen+smoke+dispensary/@38.5411617,-106.9426253,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x873f92e6665ea4c9:0xd60874f3d9ec7e4b!2m2!1d-106.9404632!2d38.5393374">500 US-50 #103, Gunnison CO 81230</a>
-                    </div>
+                    </div> */}
                 </li>
             </div>
         </div>
